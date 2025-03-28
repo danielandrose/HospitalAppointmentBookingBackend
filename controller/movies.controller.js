5f77c4f8c25077ff11321e3afb35ba8294c3e5a7
Daniel Androse P <danielandrose.p2022ece@sece.ac.in>
2025-03-28 23:52:59 +0530

    changes made

--- controller/movies.controller.js
+++ controller/movies.controller.js
@@ -1,9 +1,24 @@
+import Movie from "../models/movie.model.js"
+
 export const MovieIndex=(req,res)=>{
     res.send("Create a movie")
 }
 
-export const MovieCreate=(req,res)=>{
-    res.send("Create a movie")
+export const MovieCreate=async (req,res)=>{
+    // id,title,desc
+
+    const newMovie=new Movie({
+        title:req.body.title,
+        desc:req.body.desc
+    })
+
+    try{
+        const movie=await newMovie.save();
+        res.status(201).json(movie);
+    }
+    catch(err){
+        res.status(404).json({message : error.message})
+    }
 }
 
 export const MovieUpdate=(req,res)=>{
